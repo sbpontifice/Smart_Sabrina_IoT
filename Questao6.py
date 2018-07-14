db79b488571afa704bb601572e7124731fcdf3c1
sbpontice <sbpontifice@gmail.com>
2018-07-14 10:46:43 -0300

    7

--- Questao6.py
+++ Questao6.py
@@ -154,11 +154,9 @@ while True:
 	
 			else:
 				print "Voce acionou o controle manual! Agora pode decidir quando ligar ou desligar seus aparelhos!"
-				respostaligarele = dweet.latest_dweet(name="iplug_sabrina_q4")
-				dwligarele = respostaligarele['with'][0]['content']['rele']
-	
-				respostaligaled = dweet.latest_dweet(name="iplug_sabrina_q4")
-				dwligaled = respostaligaled['with'][0]['content']['led']
+				resposta = dweet.latest_dweet(name="iplug_sabrina_q4")
+				dwligarele = resposta['with'][0]['content']['rele']
+				dwligaled = resposta['with'][0]['content']['led']
 
 				if dwligaled ==1 and dwligarele ==1:
 					ligaled()
